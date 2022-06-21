3b70fee0934c801d58271a7d21f52ea70004ccb5
white-asteroid <shivigarg010@gmail.com>
2022-06-21 21:00:26 +0530

    fixing 2

--- client/node_modules/antd/es/card/index.js
+++
@@ -1,7 +0,0 @@
-import Grid from './Grid';
-import Meta from './Meta';
-import InternalCard from './Card';
-var Card = InternalCard;
-Card.Grid = Grid;
-Card.Meta = Meta;
-export default Card;
--- client/node_modules/antd/es/card/style/css.js
+++
@@ -1,5 +0,0 @@
-import '../../style/default.css';
-import './index.css'; // style dependencies
-
-import '../../tabs/style/css';
-import '../../skeleton/style/css';
