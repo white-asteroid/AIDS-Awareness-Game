de87aeeec895ba6435853f7ea28a4f76257e5992
Devanshi <devanshityagi3@gmail.com>
2022-07-29 20:48:14 +0530

    Game 2 changes

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "24f6fc52ac8387d0"; }
+/******/ 		__webpack_require__.h = function() { return "ff97b209b8401ef3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/component/subcomp/cardSlide.jsx
+++ client/component/subcomp/cardSlide.jsx
@@ -9,20 +9,21 @@ export default function CardSlides() {
         <div className="container-fluid p-4">
             <div className="row m-5 col-12">
                 <CardComp
-                    imgsrc="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                    ctitle="Crossword"
-                    content="Limpsum code is an examplary text whhich is used to put some random text in an empty space"
+                    imgsrc="https://thumbs.dreamstime.com/b/ready-quiz-written-notebook-marker-pen-glasses-wooden-desk-business-concept-ready-quiz-118902066.jpg"
+                    ctitle="Quiz Game"
+                    content="You will be asked different and interesting factual questions regarding AIDS. "
                 />
                 <CardComp
-                    imgsrc="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                    ctitle="Crossword"
-                    content="Limpsum code is an examplary text whhich is used to put some random text in an empty space"
+                    imgsrc="https://en.islcollective.com/preview/201711/f/clothes-memory-game-fun-activities-games-games_102602_1.jpg"
+                    ctitle="Memory game"
+                    content="In this game you have flip cards. Whichever card matches the previous one you found a pair! "
                 />
-                <CardComp
-                    imgsrc="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                    ctitle="Crossword"
-                    content="Limpsum code is an examplary text whhich is used to put some random text in an empty space"
+                 <CardComp
+                    imgsrc="https://thumbs.dreamstime.com/b/ready-quiz-written-notebook-marker-pen-glasses-wooden-desk-business-concept-ready-quiz-118902066.jpg"
+                    ctitle="Quiz Game"
+                    content="you will be asked different and interesting factual questions regarding AIDS "
                 />
+                
             </div>
         </div>
 
